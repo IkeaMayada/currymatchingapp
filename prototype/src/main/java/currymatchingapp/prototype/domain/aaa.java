6bd4c83e7b0f48d4f329bfdc668097db0b1aefcf
ikeamayada <cho.kyoung.a@fixie.jp>
2026-03-23 15:23:10 +0900

    initial committ

---
+++ prototype/src/main/java/currymatchingapp/prototype/domain/aaa.java
@@ -0,0 +1,4 @@
+package currymatchingapp.prototype.domain;
+
+public class aaa {
+}
---
+++ prototype/src/main/java/currymatchingapp/prototype/domain/model/User.java
@@ -0,0 +1,106 @@
+package currymatchingapp.prototype.domain.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String email;
+
+    @Column(name = "password_hash")
+    private String password;
+
+    private String roleName;
+
+    @Column(name = "email_verified")
+    private boolean isVerified;
+
+    @Column(name = "password_reset_token")
+    private String verificationToken;
+
+    @Column(name = "reset_token_expiry")
+    private LocalDateTime verificationTokenExpiration;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public boolean isVerified() {
+        return isVerified;
+    }
+
+    public void setVerified(boolean isVerified) {
+        this.isVerified = isVerified;
+    }
+
+    public String getVerificationToken() {
+        return verificationToken;
+    }
+
+    public void setVerificationToken(String verificationToken) {
+        this.verificationToken = verificationToken;
+    }
+
+    public LocalDateTime getVerificationTokenExpiration() {
+        return verificationTokenExpiration;
+    }
+
+    public void setVerificationTokenExpiration(LocalDateTime verificationTokenExpiration) {
+        this.verificationTokenExpiration = verificationTokenExpiration;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
